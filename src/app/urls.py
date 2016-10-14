beaf19d19644a095c7a5da0d9d74fe06dd0db257
Mo Chawich <mo.chawich@gmail.com>
2016-10-14 17:59:05 +0200

    Fix settings path

--- src/app/urls.py
+++ src/app/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from src.app import views
+from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
